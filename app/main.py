aaea2fb3007b40b0586f848311442aad15645ec0
BR19-gh <ibrahim-abdalaziz@hotmail.com>
2021-12-30 01:34:24 +0300

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template
 from flask import jsonify
 from flask import request
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS#, cross_origin
  
 app = Flask(__name__)
 CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+#app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
 def home_view():
         return "<h1>Welcome to Geeks for Geeks</h1>"
 
 @app.route("/add")
-@cross_origin()
+#@cross_origin()
 def add():
     a = request.args.get('a')
     b = request.args.get('b')
