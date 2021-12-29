2c6e02865ae7c0850adf7edf1435123e22287e6c
BR19-gh <ibrahim-abdalaziz@hotmail.com>
2021-12-30 00:57:44 +0300

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import jsonify
+from flask import request
  
 app = Flask(__name__)
  
 @app.route("/")
 def home_view():
         return "<h1>Welcome to Geeks for Geeks</h1>"
+
+@app.route("/add")
+def add():
+    a = request.args.get('a')
+    b = request.args.get('b')
+    return jsonify({"result": a+b})
