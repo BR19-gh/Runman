567cebc11c73be3dc49116ca40238163756c87d3
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2021-12-30 01:47:45 +0300

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
 from flask import jsonify
 from flask import request
-from flask_cors import CORS#, cross_origin
+#from flask_cors import CORS#, cross_origin
  
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 #app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.after_request # blueprint can also be app~~
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    return response
 
 @app.route("/")
 def home_view():
