eb2bf205fb348550a9454a450ff99face9b92264
BR19-gh <ibrahim-abdalaziz@hotmail.com>
2021-12-30 01:02:44 +0300

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from flask import jsonify
 from flask import request
+from flask_cors import CORS
  
 app = Flask(__name__)
- 
+CORS(app)
+
 @app.route("/")
 def home_view():
         return "<h1>Welcome to Geeks for Geeks</h1>"
