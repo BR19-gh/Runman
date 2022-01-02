a5bce1bfb3e3610c66b4cb401d96a2c3e91b7fd3
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2022-01-03 01:56:18 +0300

    make it better

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -789,13 +789,13 @@ document.getElementById("Voice").addEventListener("click", () => {
 document.getElementById("English").addEventListener("click", () => {
     playOrStop(menuS, VoiceOn);
     playOrStop(menuS, VoiceOn);
-    window.location.assign("index.html");
+    window.location.assign("../templates/index.html");
 });
 
 document.getElementById("Arabic").addEventListener("click", () => {
     playOrStop(menuS, VoiceOn);
     playOrStop(menuS, VoiceOn);
-    window.location.assign("indexAr.html");
+    window.location.assign("../templates/indexAr.html");
 });
 
 
