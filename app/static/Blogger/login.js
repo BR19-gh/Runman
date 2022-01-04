6f54b9c7b7c55f53b75bdfd7fe6d14fd583de4c6
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2022-01-04 03:16:39 +0300

    make it better

--- app/static/Blogger/login.js
+++ app/static/Blogger/login.js
@@ -1,44 +1,37 @@
-
-
-
 let checkbox = document.getElementById('checkbox');
 
 
 
-if(localStorage.getItem('rememberMeVal')==1){
-document.getElementById('username').value=localStorage.getItem('username');
-document.getElementById('password').value=localStorage.getItem('password');
-}
-else if(localStorage.getItem('rememberMeVal')==0){
-document.getElementById('username').value="";
-document.getElementById('password').value="";
+if (localStorage.getItem('rememberMeVal') == 1) {
+    document.getElementById('username').value = localStorage.getItem('username');
+    document.getElementById('password').value = localStorage.getItem('password');
+} else if (localStorage.getItem('rememberMeVal') == 0) {
+    document.getElementById('username').value = "";
+    document.getElementById('password').value = "";
 }
 
 
-function login(){
+function login() {
 
     rememberMe();
-    if(document.getElementById('username').value=='admin'&&document.getElementById('password').value=='admin'){
-        localStorage.setItem('isUserAdmin',1);
-        window.location.assign('home.html');
-    }
-
-    else{
-        localStorage.setItem('isUserAdmin',0);
-        window.location.assign('home.html'); 
+    if (document.getElementById('username').value == 'admin' && document.getElementById('password').value == 'admin') {
+        localStorage.setItem('isUserAdmin', 1);
+        window.location.assign('/home');
+    } else {
+        localStorage.setItem('isUserAdmin', 0);
+        window.location.assign('/home');
     }
 
 }
-document.getElementById('loginBtn').addEventListener('click',()=>{login()})
+document.getElementById('loginBtn').addEventListener('click', () => { login() })
 
-function rememberMe(){
-    if(checkbox.checked==true){
-        localStorage.setItem('username',document.getElementById('username').value);
-        localStorage.setItem('password',document.getElementById('password').value);
-        localStorage.setItem('rememberMeVal',1)
-    }
-    else{
-        localStorage.setItem('rememberMeVal',0) 
+function rememberMe() {
+    if (checkbox.checked == true) {
+        localStorage.setItem('username', document.getElementById('username').value);
+        localStorage.setItem('password', document.getElementById('password').value);
+        localStorage.setItem('rememberMeVal', 1)
+    } else {
+        localStorage.setItem('rememberMeVal', 0)
     }
 
 }
