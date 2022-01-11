78a3b38e0bd5551b3317ae596069d0bf63e5f634
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2022-01-11 06:38:37 +0300

    initial commit

--- app/static/Runman/script.js
+++ app/static/Runman/script.js
@@ -524,7 +524,7 @@ var timer;
 cloudChangerVal = setInterval(() => {
     let cloudChooserVal = Math.floor(Math.random() * 3) + 1;
     if (cloudChooserVal == 1) { cloud.style.backgroundImage = 'url("../../static/Runman/img/cloud/1.png")'; } else if (cloudChooserVal == 2) { cloud.style.backgroundImage = 'url("../../static/Runman/img/cloud/2.png")'; } else { cloud.style.backgroundImage = 'url("../../static/Runman/img/cloud/3.png")'; }
-}, 20000);
+}, 21000);
 
 
 //start
