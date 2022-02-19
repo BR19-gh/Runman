15ba46a72fe1c7908d66ee61e5c3fa9dc8053b35
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2022-02-20 00:51:09 +0300

    adding err500 page

--- app/main.py
+++ app/main.py
@@ -446,6 +446,7 @@ def ratelimit_handler(e):
 
 @app.errorhandler(404)
 def ratelimit_handler(e):
+    print(msg)
     msg = '{"msg": f"Error 404: the requested URL was not found on the server. If you entered the URL manually please check your spelling and try again", "statCode": 404}'
     return render_template('errPages/404err.html', msg=msg)
 
